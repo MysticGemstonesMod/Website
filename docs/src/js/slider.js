6471acf1c16a2fd997ac625a227fe09d29288c2e
Dekovicc <dekovicc@gmail.com>
2021-11-10 01:41:28 +0100

    Added slider and reworked codebase

---
+++ docs/src/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.hero_slider').slick({            
+        draggable:false,         
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1000,
+        arrows : false,
+        slidesToShow: 1,
+        slidesToScroll: 1                
+    });
+});
