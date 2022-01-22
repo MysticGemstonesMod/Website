16340d5c44ad66f295fef6e197d4437abc11bcc6
Domza64 <juricicdominik@protonmail.com>
2022-01-22 19:31:55 +0100

    Organised main page a bit

--- docs/src/js/slider.js
+++ docs/src/js/slider.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $('.hero_slider').slick({            
-        draggable:false,         
+    $('.hero_slider').slick({
+        draggable:true,
         autoplay: true,
         autoplaySpeed: 5000,
         speed: 1000,
         arrows : false,
         slidesToShow: 1,
-        slidesToScroll: 1                
+        slidesToScroll: 1
     });
 });
